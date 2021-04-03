d1e9b05c371665bf9f592bb50a1a5ff81fc9758e
yinstm124 <yingstm@hotmail.com>
2021-04-03 22:26:19 +0700

    ss

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const path = require('path')
-
-console.log('server .js')
-
-
-app.use(express.static('/Users/sutimarpengpinij/Desktop/204361_OOD/scan-manage/manage-score-web/angularapp'))
-
-console.log('app use')
-
-app.get('/', (req,res) => {
-    // res.send('test')
-    res.sendFile('index.html', {root: '/Users/sutimarpengpinij/Desktop/204361_OOD/scan-manage/manage-score-web/angularapp/'})
-})
-
-app.listen(process.env.PORT || 8080, () => {
-    console.log('port is working')
-})
+app.use(express.static(__dirname + '/dist/manage-score-web'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/manage-score-web/index.html'));}
+);
+app.listen(process.env.PORT || 8080);
